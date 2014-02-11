5e47c2d47755468fc71e0a54644e4049f7e8e2ce
Carlos Mantilla <carlos@gizra.com>
2014-02-11 16:37:51 +0200

    Adding unescaping of HTML entities

--- Gruntfile.js
+++ Gruntfile.js
@@ -1024,7 +1024,8 @@ module.exports = function (grunt) {
             // Remove extension value from the name.
             item.name = item.name.replace(attachment.format, '');
             // URL Encoding.
-            item.name = decodeURI(item.name);
+            item.name = he.unescape(item.name);
+            item.src = he.unescape(item.src);
             attachmentsParsed.images.push(item);
           }
           else if (attachment.format === '.mp3') {
